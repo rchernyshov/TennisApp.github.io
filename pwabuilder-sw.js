e4b4e5acef2b8294a4ed64e2ac91a086bb9dd28f
Roman <tchernyshoff.roman2015@gmail.com>
2024-07-20 17:45:28 +0300

    add animation and transitions

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -10,9 +10,8 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// NetworkFirst strategy
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp('/.*'), // Регулярное выражение для маршрутизации
   new workbox.strategies.NetworkFirst({
     cacheName: CACHE,
     plugins: [
@@ -20,30 +19,6 @@ workbox.routing.registerRoute(
         maxEntries: 50, // Максимальное количество элементов в кэше
         maxAgeSeconds: 30 * 24 * 60 * 60, // Кэш действителен в течение 30 дней
       }),
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [0, 200], // Кэшируемые ответы с этими статусами
-      }),
     ],
   })
 );
-
-// Precache fallback for offline support
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll([
-        '/offline.html',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match('/offline.html');
-      })
-    );
-  }
-});
